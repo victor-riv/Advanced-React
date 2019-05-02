dd48b284da4a861f2d51bf5cdb27674ab64878ba
Victor Rivera <verm2405@gmail.com>
2019-05-01 23:19:37 -0400

    going through first 2 modules

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hey!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Sell = () => {
+  return (
+    <div>
+      <p>Sell!</p>
+    </div>
+  );
+};
+
+export default Sell;
