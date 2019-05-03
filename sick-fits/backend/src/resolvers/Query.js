a760f891d8435e065dadd1313c394e799d6fb5f8
Victor Rivera <verm2405@gmail.com>
2019-05-02 23:26:49 -0400

    End of module 3
    Set up graphql yoga and prisma.
    
    Learned about the data flow and queries and mutations and all that.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // async items(parent, args, ctx, info) {
+  //   console.log("Getting Items!");
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+  items: forwardTo("db")
+};
 
 module.exports = Query;
